5cbd05cf3daa04a2a74363df8a5859da96994734
NicholasCF <nicholas.cristian@yahoo.com>
2019-10-05 21:06:15 +0800

    Define controller rspec

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe BookingsController, type: :controller do
   describe 'GET bookings#index' do
-    it 'loads index' 
-      #get :index
-      #expect(response).to render_template("index")
+    it 'loads index' do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 end
