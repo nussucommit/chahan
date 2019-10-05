2e646cf3b1d416fa9ecb3eed2663f0a67f1ecbbd
NicholasCF <nicholas.cristian@yahoo.com>
2019-10-05 19:57:03 +0800

    Add basic rspec outline

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -1,1 +1,7 @@
 require 'rails_helper'
+
+RSpec.describe BookingsController, type: :controller do
+  describe 'GET bookings#index' do
+    it 'loads index' 
+  end
+end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -13,3 +13,10 @@
 #  updated_at :datetime         not null
 #
 
+RSpec.describe Booking, type: :model do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:category) }
+  it { should validate_presence_of(:items) }
+  it { should validate_numericality_of(:quantity).is_greater_than 0 }
+end
