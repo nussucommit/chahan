88b7eb5c732217ca4dd65d2b0db249f8af84ad05
NicholasCF <nicholas.cristian@yahoo.com>
2019-10-05 20:20:57 +0800

    Define index in Booking controller

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe BookingsController, type: :controller do
   describe 'GET bookings#index' do
     it 'loads index' 
+      #get :index
+      #expect(response).to render_template("index")
   end
 end
