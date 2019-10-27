3d80db87885eaa1f82e37084791f64faa1485d41
NicholasCF <nicholas.cristian@yahoo.com>
2019-10-27 17:40:32 +0800

    Add controller method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,23 @@
 class BookingsController < ApplicationController
+  # Disable authenticity token verification for now
+  skip_before_action :verify_authenticity_token
+
   def index
     @bookings = Booking.all
+  end
+
+  def create
+    booking = Booking.new booking_params
+
+    if booking.save
+      #placeholder
+    else
+      #placeholder
+    end
+  end
+
+private
+  def booking_params
+    params.require(:booking).permit(:name, :email, :category, :items, :quantity, :purpose)
   end
 end
