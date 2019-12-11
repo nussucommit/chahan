ad51045ec550d885e7ba1668c9e78203905bc48f
NicholasCF <nicholas.cristian@yahoo.com>
2019-12-11 13:49:41 +0800

    Add status column to Booking table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_05_092540) do
+ActiveRecord::Schema.define(version: 2019_12_11_054726) do
 
   create_table "bookings", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_10_05_092540) do
     t.text "purpose"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
   end
 
 end
