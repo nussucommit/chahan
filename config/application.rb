fd740d5781b6fe02fe0ff1bb308cb05a17a23e63
Zhong Jun <zhongjun@jorked.com>
2020-01-19 02:06:11 +0800

    Enable CORS gem to receive booking information

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,13 @@ module Chahan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'nussu.org.sg', 'www.nussu.org.sg'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
