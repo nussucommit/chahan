84190ba7fde995704c772a97234b7a1734ea8aaa
NicholasCF <nicholas.cristian@yahoo.com>
2020-02-08 11:22:58 +0800

    Implement controller methods

--- app/mailers/generic_mailer.rb
+++ app/mailers/generic_mailer.rb
@@ -2,9 +2,25 @@ class GenericMailer < ApplicationMailer
   default from: %("NUSSU Logistics" <test@nussulogistics.com>)
 
   def reject(booking)
+    @items = booking.items
+    @category = booking.category
+    @quantity = booking.quantity
+    @purpose = booking.purpose
 
+    mail(to: get_recipient(booking),
+         subject: "Booking rejected")
   end
 
   def accept(booking)
+    mail(to: get_recipient(booking),
+         subject: "Booking accepted")
   end
+
+  private
+
+  def get_recipient(booking)
+    %("#{booking.name}" <#{booking.email}>)
+  end
+
+
 end
