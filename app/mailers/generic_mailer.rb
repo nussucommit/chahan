0e737c2876f11848884be91ad720539cef0008e9
NicholasCF <nicholas.cristian@yahoo.com>
2020-02-15 11:50:33 +0800

    Clean up method and their names

--- app/mailers/generic_mailer.rb
+++ app/mailers/generic_mailer.rb
@@ -1,19 +1,35 @@
 class GenericMailer < ApplicationMailer
   default from: %("NUSSU Logistics" <test@nussulogistics.com>)
 
-  def reject(booking)
+  def reject
+    booking = params[:booking]
+    @items = booking.items
+    @category = booking.category
+    @quantity = booking.quantity
+    @purpose = booking.purpose
+
+    puts '======================='
+    puts 'rejected'
+
+    mail(to: get_recipient(booking),
+         subject: "Booking rejected",
+         template_path: 'generic_mailer',
+         template_name: 'reject')
+    
+    
+  end
+
+  def approve
+    booking = params[:booking]
     @items = booking.items
     @category = booking.category
     @quantity = booking.quantity
     @purpose = booking.purpose
 
     mail(to: get_recipient(booking),
-         subject: "Booking rejected")
-  end
-
-  def accept(booking)
-    mail(to: get_recipient(booking),
-         subject: "Booking accepted")
+         subject: "Booking approved",
+         template_path: 'generic_mailer',
+         template_name: 'approve')
   end
 
   private
