0ffdef8d0e397f7f63a58d9afe4354ff703a6a97
NicholasCF <nicholas.cristian@yahoo.com>
2020-02-22 10:56:05 +0800

    Add email template controller methods

--- app/controllers/email_template_controller.rb
+++ app/controllers/email_template_controller.rb
@@ -1,2 +1,23 @@
 class EmailTemplateController < ApplicationController
+  def index
+    @templates = EmailTemplate.all
+  end
+
+  def create
+    @template = EmailTemplate.new template_params
+
+    if @template.save
+      redirect_to email_template_index_path,
+                  notice: 'Template succesfully saved!'  
+    else
+      redirect_to email_template_index_path,
+                  alert: 'Could not save template!'
+    end
+  end
+
+  private
+
+  def template_params
+    params.require(:email_template).permit(:content)
+  end
 end
