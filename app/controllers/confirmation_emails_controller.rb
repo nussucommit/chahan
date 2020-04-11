28878c20f52acd03e2ffab83923919a58e458f44
NicholasCF <nicholas.fernando@u.nus.edu>
2020-04-11 11:27:38 +0800

    Add page for making new emails

--- app/controllers/confirmation_emails_controller.rb
+++ app/controllers/confirmation_emails_controller.rb
@@ -1,2 +1,23 @@
 class ConfirmationEmailsController < ApplicationController
+  def new
+    @email = ConfirmationEmail.new
+  end
+
+  def create
+    email = ConfirmationEmail.new email_params
+    if email.save
+      redirect_to bookings_path, notice: "Sent email!"
+    else
+      @email = email
+      flash.now[:alert] = "Failed to send email:
+                          #{email.errors.full_messages.join(', ')}"
+      render new_confirmation_email_path
+    end
+  end
+
+  private
+
+  def email_params
+    params.require(:confirmation_email).permit(:recipient, :subject, :content)
+  end
 end
