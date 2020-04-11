74165cd7e8ac8f483486b94b596a5f56ee068836
NicholasCF <nicholas.fernando@u.nus.edu>
2020-04-11 12:30:44 +0800

    Add simple validation rspec

--- spec/models/confirmation_email_spec.rb
+++ spec/models/confirmation_email_spec.rb
@@ -13,5 +13,6 @@
 require 'rails_helper'
 
 RSpec.describe ConfirmationEmail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :content }
+  it { should validate_presence_of :subject }
 end
--- spec/models/email_template_spec.rb
+++ spec/models/email_template_spec.rb
@@ -12,5 +12,6 @@
 require 'rails_helper'
 
 RSpec.describe EmailTemplate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :content }
+  it { should validate_presence_of :name }
 end
